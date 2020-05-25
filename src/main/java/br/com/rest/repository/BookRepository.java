90a8daa1fb3852e86cb59e87ed8e48fa8390a632
junior.capra <acaprajr@gmail.com>
2020-05-25 10:17:44 -0300

    Commit inicial

---
+++ src/main/java/br/com/rest/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package br.com.rest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.rest.data.model.Book;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long>{
+
+}
